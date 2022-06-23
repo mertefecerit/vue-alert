bc285b97baf4326acf77107e9adeb2c7233fcef8
mertefecerit <mertefecerit@gmail.com>
2022-06-23 06:15:24 +0300

    Alert Component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Alert from "./components/Alert.vue";
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 justify-center items-center min-h-screen">
+    <Alert type="primary">
+      Burası primary alert kutusu
+    </Alert>
+    <Alert type="warning">
+      Burası primary warning kutusu
+    </Alert> 
+    <Alert type="danger">
+      Burası primary danger kutusu
+    </Alert> 
+    <Alert type="info">
+      Burası primary info kutusu
+    </Alert> 
+  </div>
+</template>
---
+++ src/components/Alert.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex p-2 rounded w-full" :class="alertStyle">
+      <slot></slot>
+    </div>
+</template>
+<script setup>
+import { computed } from '@vue/reactivity';
+
+    const props = defineProps({
+        type:{
+            default:"primary",
+            type: String
+        }
+    })
+
+    const alertStyle = computed(() => {
+        return [
+            {'primary':props.type === "primary"},
+            {'warning':props.type === "warning"},
+            {'danger':props.type === "danger"},
+            {'info':props.type === "info"},
+        ]
+    })
+</script>
+
+<style scoped>
+    .primary{
+        @apply bg-blue-200 border border-blue-500 text-blue-700
+    }
+    .warning{
+        @apply bg-yellow-200 border border-yellow-500 text-yellow-700
+    }
+    .danger{
+        @apply bg-red-200 border border-red-500 text-red-700
+    }
+    .info{
+        @apply bg-gray-200 border border-gray-500 text-gray-700
+    }
+</style>
