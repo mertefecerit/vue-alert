839e31628149209f1e82fd122a6fb38203a1412f
Mert Efe Cerit <mertefecerit@gmail.com>
2022-06-23 09:57:58 +0300

    UPDATE

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,25 @@ import Alert from "./components/Alert.vue";
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 justify-center items-center min-h-screen">
-    <Alert type="primary">
-      Burası primary alert kutusu
-    </Alert>
-    <Alert type="warning">
-      Burası primary warning kutusu
-    </Alert> 
-    <Alert type="danger">
-      Burası primary danger kutusu
-    </Alert> 
-    <Alert type="info">
-      Burası primary info kutusu
-    </Alert> 
+  <div class="flex justify-center items-center min-h-screen bg-gray-200">
+    <div class="w-80 flex flex-col gap-2 bg-white p-2 rounded">
+      <h1 class="text-center">Alert Components</h1>
+      <hr>
+      <Alert type="primary">
+        Burası primary alert kutusu Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, molestiae.
+      </Alert>
+      <Alert type="warning">
+        Burası primary warning kutusu Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, molestiae.
+      </Alert>
+      <Alert type="success">
+        Burası primary success kutusu Lorem ipsum dolor sit amet consectetur
+      </Alert> 
+      <Alert type="danger">
+        Burası primary danger kutusu Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, molestiae.
+      </Alert> 
+      <Alert type="info">
+        Burası primary info kutusu
+      </Alert> 
+    </div>
   </div>
 </template>
--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="flex p-2 rounded w-full" :class="alertStyle">
-      <slot></slot>
-    </div>
+    <div class="flex rounded w-full items-center" :class="alertStyle">
+            <div>
+                <svg v-if="type === 'primary'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z"/></svg>
+                <svg v-if="type === 'success'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM371.8 211.8C382.7 200.9 382.7 183.1 371.8 172.2C360.9 161.3 343.1 161.3 332.2 172.2L224 280.4L179.8 236.2C168.9 225.3 151.1 225.3 140.2 236.2C129.3 247.1 129.3 264.9 140.2 275.8L204.2 339.8C215.1 350.7 232.9 350.7 243.8 339.8L371.8 211.8z"/></svg>
+                <svg v-if="type === 'danger'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM175 208.1L222.1 255.1L175 303C165.7 312.4 165.7 327.6 175 336.1C184.4 346.3 199.6 346.3 208.1 336.1L255.1 289.9L303 336.1C312.4 346.3 327.6 346.3 336.1 336.1C346.3 327.6 346.3 312.4 336.1 303L289.9 255.1L336.1 208.1C346.3 199.6 346.3 184.4 336.1 175C327.6 165.7 312.4 165.7 303 175L255.1 222.1L208.1 175C199.6 165.7 184.4 165.7 175 175C165.7 184.4 165.7 199.6 175 208.1V208.1z"/></svg>
+                <svg v-if="type === 'warning'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"/></svg>
+                <svg v-if="type === 'info'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c17.67 0 32 14.33 32 32c0 17.67-14.33 32-32 32S224 177.7 224 160C224 142.3 238.3 128 256 128zM296 384h-80C202.8 384 192 373.3 192 360s10.75-24 24-24h16v-64H224c-13.25 0-24-10.75-24-24S210.8 224 224 224h32c13.25 0 24 10.75 24 24v88h16c13.25 0 24 10.75 24 24S309.3 384 296 384z"/></svg>
+            </div>
+            <div>
+                <slot></slot>
+            </div>
+        </div>
 </template>
 <script setup>
 import { computed } from '@vue/reactivity';
@@ -18,22 +27,44 @@ import { computed } from '@vue/reactivity';
             {'primary':props.type === "primary"},
             {'warning':props.type === "warning"},
             {'danger':props.type === "danger"},
+            {'success':props.type === "success"},
             {'info':props.type === "info"},
         ]
     })
 </script>
 
 <style scoped>
+    svg {
+        @apply w-6
+    }
     .primary{
-        @apply bg-blue-200 border border-blue-500 text-blue-700
+        @apply bg-blue-200 border border-blue-500 text-blue-700 divide-x divide-blue-500
+    }
+    .primary div{
+        @apply p-4 fill-blue-700
     }
     .warning{
-        @apply bg-yellow-200 border border-yellow-500 text-yellow-700
+        @apply bg-yellow-200 border border-yellow-500 text-yellow-700 divide-x divide-yellow-500
+    }
+    .warning div{
+        @apply p-4 fill-yellow-700
     }
     .danger{
-        @apply bg-red-200 border border-red-500 text-red-700
+        @apply bg-red-200 border border-red-500 text-red-700 divide-x divide-red-500
+    }
+    .danger div{
+        @apply p-4 fill-red-700
+    }
+    .success{
+        @apply bg-green-200 border border-green-500 text-green-700 divide-x divide-green-500
+    }
+    .success div{
+        @apply p-4 fill-green-700
     }
     .info{
-        @apply bg-gray-200 border border-gray-500 text-gray-700
+        @apply bg-gray-200 border border-gray-500 text-gray-700 divide-x divide-gray-500
+    }
+    .info div{
+        @apply p-4 fill-gray-700
     }
 </style>
